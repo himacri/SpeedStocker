82b9a1146ef30e110d70a340999718dc3832508d
宮浦達信 <>
2021-01-01 17:05:48 +0900

    nankai yareba iinoda

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 worker: python -u run-worker.py
-web: gunicorn main:app --log-file
+web: gunicorn main:app
--- main.py
+++ main.py
@@ -204,5 +204,5 @@ def write_csv() :
 # ------------------------------------------------------------------
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=False,host='0.0.0.0', port=4000)
+    app.run(debug=False,host='0.0.0.0', port=4000, threaded=True)
 # ------------------------------------------------------------------
